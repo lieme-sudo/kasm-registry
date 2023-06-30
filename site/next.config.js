c1cc48f9a79fa7a4bde1fa4002a9106bcf8c0abd
lieme-sudo <66252661+lieme-sudo@users.noreply.github.com>
2023-06-30 16:04:42 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'test evironment',
+    description: 'My test environment for KASM images.',
+    icon: 'https://lieme-sudo.github.io/kasm-registry/1.0/icon.png',
+    listUrl: 'https://lieme-sudo.github.io/kasm-registry/',
+    contactUrl: 'https://lieme-sudo.github.io/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
